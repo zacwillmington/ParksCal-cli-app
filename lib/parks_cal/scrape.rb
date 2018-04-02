c4c3fc78d6b0ba1b654966a2168d96d9fc30402c
Zac Willmington <zacwillmington@Zacs-Air.attlocal.net>
2018-04-02 10:09:21 -0700

    Built scrape_name method to create an array of names of each park

--- lib/parks_cal/scrape.rb
+++ lib/parks_cal/scrape.rb
@@ -1,19 +1,24 @@
 require "open-uri"
 require "nokogiri"
-require 'pry'
+require "pry"
 
 class ParksCal::Scrape
-    attr_accessor :parks_site_url, :park_name
+    attr_accessor :parks_site_url, :park_names
 
     def initialize
         @parks_site_url = "https://www.nps.gov/state/ca/list.htm?program=parks"
     end
 
     def scrape_names
-        #loop through pages h3 elements selecting name of Locations.
         html = open(@parks_site_url)
         doc = Nokogiri::HTML(html)
         #binding.pry
+        park_names = doc.search(".table-cell")
+        parks = park_names.collect do |name|
+            name.css("h3 a").text
+        end
+        @park_names = parks.reject(&:empty?)# Find out why .css was returning empty elements
+        binding.pry
     end
 
     def scrape_data(a)
